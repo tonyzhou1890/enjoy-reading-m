0a617e298d9c6cfe6a1e4c799d0a34a6d50a7c94
Tony <1332988826@qq.com>
2020-04-06 21:25:25 +0800

    解决无法正确退出的问题 Cookies.remove 需要传入设置时的参数

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,11 +2,11 @@
 module.exports = {
   NODE_ENV: '"production"',
   // base api
-  VUE_APP_BASE_API: 'http://api-node.tony93.top',
+  VUE_APP_BASE_API: '"https://api-node.tony93.top"',
   // 图片等资源的域名地址
-  VUE_APP_STORE: 'http://store.tony93.top',
+  VUE_APP_STORE: '"https://store.tony93.top"',
   // 注册中心地址
-  VUE_APP_REGISTER: 'https://register.tony93.top',
+  VUE_APP_REGISTER: '"https://register.tony93.top"',
   // 阅读器地址
-  VUE_APP_READER: 'https://reader.tony93.top'
+  VUE_APP_READER: '"https://reader.tony93.top"'
 }
